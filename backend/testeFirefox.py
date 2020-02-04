1c898775f29f778e8459fbaae4e4317060774706
Gabriel Lopes <gabriellopes@MacBook-Air-de-Gabriel.local>
2020-02-04 19:40:15 -0300

    funfou

--- backend/testeFirefox.py
+++ backend/testeFirefox.py
@@ -1,24 +1,19 @@
 from selenium import webdriver
 import time
-from flask import Flask
+import os
+from flask import Flask, request
 import json
 app = Flask(__name__)
 
 global contatos
-contatos1 = [{
-  "contato":"Gabriel",
-  "nome": "Gabriel",
-  "numero": "83996554938"
-}, {
-  "contato":"Gabriel2",
-  "nome": "Gabriel2",
-  "numero": "839965549382"
-}]
 
 @app.route("/recuperarContatos", methods=['GET'])
 def recuperarContatos():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    arq = open(dir_path + '/contatos.json', 'r')
+    saudacao = json.load(arq)
     response = app.response_class(
-        response=json.dumps(contatos1),
+        response=json.dumps(saudacao),
         status=200,
         mimetype='application/json')
     return response
@@ -29,31 +24,29 @@ def atualizarJson(listaContatos):
     contatosEscrita = open('contatos.json', 'r+')
 
 
-
-def enviarMensagens(listContatos, mensagem):
-    for contatoList in listContatos:
+@app.route("/enviarMensagem", methods=['POST'])
+def enviarMensagens():
+    content = request.json
+    for contatoList in content.listaContatos:
         findContato = driver.find_element_by_xpath(f"//input[@class='_2zCfw copyable-text selectable-text']")
         time.sleep(1)
         findContato.send_keys(contatoList)
         time.sleep(1)
-        contato_pesquisado = driver.find_element_by_xpath(f"//span[@title='" + i + "']")
+        contato_pesquisado = driver.find_element_by_xpath(f"//span[@title='" + contatoList + "']")
         time.sleep(1)
         contato_pesquisado.click()
         chat_box = driver.find_element_by_class_name('_13mgZ')
         time.sleep(1)
         chat_box.click()
-        chat_box.send_keys(mensagem)
+        chat_box.send_keys(content.mensagem)
         botao_enviar = driver.find_element_by_xpath("//span[@data-icon='send']")
         time.sleep(1)
         botao_enviar.click()
         time.sleep(1)
-
-        # driver.find_element_by_name("q").send_keys("cheese" + Keys.RETURN)
-        # wait = WebDriverWait(driver, 10)"
-        # wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>a")))
-        # results = driver.find_elements_by_css_selector("h3>a")
-        # for i, result in results.iteritems():
-        # print("#{}: {} ({})".format(i, result.text, result.get_property("href")))
+    response = app.response_class(
+        status=200,
+        mimetype='application/json')
+    return response
 
 @app.route("/init", methods=['GET'])
 def init():
@@ -61,8 +54,9 @@ def init():
     driver = webdriver.Chrome()
     driver.get("https://web.whatsapp.com")
     time.sleep(5)
-    return True
-
+    response = app.response_class(
+        status=200,
+        mimetype='application/json')
+    return response
 if __name__ == "__main__":
     app.run()
-
