ca3f2bbaf7908004f8d8b9722d346007321745dd
Gabriel Lopes <gabriellopes@MacBook-Air-de-Gabriel.local>
2020-02-24 11:54:52 -0300

    ˜ajustebackend˜

--- backend/setup.py
+++ backend/setup.py
@@ -1,7 +1,7 @@
 from cx_Freeze import setup, Executable
 setup(
-    name = "MoveZap",
-    version = "1.0.0",
+    name = "moveZapRest",
+    version = "1.1.0",
     options = {"build_exe": {
         'packages': ["selenium","time", "sqlite3", "flask","json","random", "os"],
         'include_msvcr': True,
