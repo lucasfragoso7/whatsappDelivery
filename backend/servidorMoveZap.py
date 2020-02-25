05719458a96f0cb3c005f15aaad74672ba878065
Gabriel Lopes <gabriellopes@MacBook-Air-de-Gabriel.local>
2020-02-25 19:43:21 -0300

    ajusteArquivos

--- backend/servidorMoveZap.py
+++ backend/servidorMoveZap.py
@@ -10,13 +10,23 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-global contatos
+@app.route("/salvarArquivos", methods=['POST'])
+def salvarArquivos():
+    content = request.json
+    connBd = sqlite3.connect('bancoDeDados.db')
+    nomeArquivo = content["nomeArquivo"]
+    arquivo = open(nomeArquivo)
+    arquivo = json.load(arquivo)
+    for contato in arquivo:
+        salvarNovoContato(contato,connBd)
+    connBd.close()
+
 
 @app.route("/recuperarContatos", methods=['GET'])
 def recuperarContatos():
     listContatos = {}
     listaContatosJson = []
-    connBd = sqlite3.connect('C:/bancoDeDados.db')
+    connBd = sqlite3.connect('bancoDeDados.db')
     for contato in connBd.execute('select * from contatos'):
         listContatos = {"nome": contato[1], "id": contato[0],
                         "telefone": contato[2],
@@ -33,7 +43,7 @@ def recuperarContatos():
 def apagarContato(id):
     id = str(id)
     aspas = "'"
-    connBd = sqlite3.connect('C:/bancoDeDados.db')
+    connBd = sqlite3.connect('bancoDeDados.db')
     connBd.execute('delete from contatos where id_contato = ' + aspas + id + aspas)
     connBd.commit()
     connBd.close()
@@ -44,7 +54,7 @@ def apagarContato(id):
 
 @app.route("/salvarContato", methods=['POST'])
 def salvarContato():
-    connBd = sqlite3.connect('C:/bancoDeDados.db')
+    connBd = sqlite3.connect('bancoDeDados.db')
     content = request.json
     if('id' not in content):
        salvarNovoContato(content, connBd)
@@ -106,7 +116,7 @@ def enviarMensagens():
                 chat_box.click()
                 time.sleep(1)
                 anexar = driver.find_element_by_xpath(f"//input[@type='file']")
-                anexar.send_keys(os.path.abspath("C:/" + content["nomeArquivo"]))
+                anexar.send_keys(os.path.abspath("" + content["nomeArquivo"]))
                 time.sleep(2)
                 botaoEnviar = driver.find_element_by_xpath(f"//div[@class='_1g8sv NOJWi']")
                 botaoEnviar.click()
